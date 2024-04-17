90d61ba89025eaa6bf3a787b7dedb386c325b243
Luan <luan086@gmail.com>
2024-04-17 19:56:41 +0200

    Modified the grid with correct center align

--- script/script.js
+++ script/script.js
@@ -1,11 +1,11 @@
 "use strict";
 
 function createGrid() {
-    let container = document.querySelector(".container");
+    let grid = document.querySelector(".grid");
     for (let i = 0; i < 16; i++) {
         let row = document.createElement("div");
         row.className = "row";
-        container.appendChild(row);
+        grid.appendChild(row);
         for (let j = 0; j < 16; j++) {
             let div = document.createElement("div");
             div.className = "col";
