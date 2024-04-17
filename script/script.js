d59cdbb8ae2f832bd1e86c3fbd41bd6c72f89600
Luan <luan086@gmail.com>
2024-04-17 19:20:39 +0200

    Created a 16x16 square grid

--- script/script.js
+++ script/script.js
@@ -1,2 +1,17 @@
 "use strict";
 
+function createGrid() {
+    let container = document.querySelector(".container");
+    for (let i = 0; i < 16; i++) {
+        let row = document.createElement("div");
+        row.className = "row";
+        container.appendChild(row);
+        for (let j = 0; j < 16; j++) {
+            let div = document.createElement("div");
+            div.className = "col";
+            row.appendChild(div);
+        }
+    }
+}
+
+createGrid();
