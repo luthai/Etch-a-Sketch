a157c7bacb6d2d4db848055d08c52d0ac5fd3e6e
Luan <luan086@gmail.com>
2024-04-20 12:27:24 +0200

    added deeper color when revisiting square

--- script/script.js
+++ script/script.js
@@ -7,11 +7,21 @@ function createGrid() {
         row.className = "row";
         grid.appendChild(row);
         for (let j = 0; j < 16; j++) {
-            let div = document.createElement("div");
-            div.className = "col";
-            row.appendChild(div);
+            let col = document.createElement("col");
+            col.className = "col";
+            row.appendChild(col);
         }
     }
 }
 
 createGrid();
+
+const mySketch = document.querySelectorAll(".col");
+mySketch.forEach((elem) => {
+    elem.addEventListener("mouseenter", (e) => {
+        console.log(e.target.style.opacity);
+        if (!e.target.style.opacity) e.target.style.opacity = 0;
+        e.target.style.opacity = parseFloat(e.target.style.opacity) + 0.2;
+        if (e.target.style.opacity > 1) e.target.style.opacity = 1;
+    })
+})
