5ed56ea0b0eafa1866c3807f643da1eef2abf98b
Luan <luan086@gmail.com>
2024-04-21 10:14:58 +0200

    Added a new grid button, and color to squares, and limit 100 squares

--- script/script.js
+++ script/script.js
@@ -1,27 +1,64 @@
 "use strict";
 
 function createGrid() {
+    let grid_size = userGridChoice();
     let grid = document.querySelector(".grid");
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < grid_size; i++) {
         let row = document.createElement("div");
         row.className = "row";
         grid.appendChild(row);
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < grid_size; j++) {
             let col = document.createElement("col");
             col.className = "col";
             row.appendChild(col);
         }
     }
+
+    initSquares();
+}
+
+function resetGrid() {
+    let grid = document.querySelector(".grid");
+    grid.replaceChildren();
 }
 
 createGrid();
 
-const mySketch = document.querySelectorAll(".col");
-mySketch.forEach((elem) => {
-    elem.addEventListener("mouseenter", (e) => {
-        console.log(e.target.style.opacity);
-        if (!e.target.style.opacity) e.target.style.opacity = 0;
-        e.target.style.opacity = parseFloat(e.target.style.opacity) + 0.2;
-        if (e.target.style.opacity > 1) e.target.style.opacity = 1;
+function initSquares() {
+    const mySketch = document.querySelectorAll(".col");
+    mySketch.forEach((elem) => {
+        elem.style.backgroundColor = randomColor();
+        elem.addEventListener("mouseenter", (e) => {
+            if (!e.target.style.opacity) e.target.style.opacity = 0;
+            e.target.style.opacity = parseFloat(e.target.style.opacity) + 0.1;
+            if (e.target.style.opacity > 1) e.target.style.opacity = 1;
+        })
     })
-})
+}
+
+function randomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function userGridChoice() {
+    let grid_number = 0;
+    do {
+        grid_number = prompt("how many number of squares per side for the grid(X * X): ");
+        if (grid_number > 100) {
+            alert("Error: maximum squares per side is 100, please choose again.");
+        }
+    } while (grid_number > 100)
+
+    return grid_number;
+}
+
+const btnNewGrid = document.querySelector(".btnNewGrid");
+btnNewGrid.addEventListener("click", (e) => {
+    resetGrid();
+    createGrid();
+});
